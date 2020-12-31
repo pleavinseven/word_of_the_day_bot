22a49de58a2b406fa77a6173218ec6d1ae8b3f1a
pleav <pleavinseven@gmail.com>
2020-12-31 17:44:59 +0100

    Text file with list of English words to be translated-

---
+++ redditbot.py
@@ -0,0 +1,28 @@
+import praw
+import random
+from googletrans import Translator
+from PyDictionary import PyDictionary
+
+translator = Translator()
+dictionary = PyDictionary()
+
+word_list = open('word_list.text', 'r').read().split(' ')
+
+def test_post():
+    reddit = 'redditpraw_credentials' # TODO: github secret
+
+    eng_word = random.choice(word_list)
+    translate_word = translator.translate(eng_word, dest='cy', src='auto') # all info on translated word, too much info.
+    welsh_word = translate_word.text  # translated word only.
+    if welsh_word == eng_word:
+        test_post()
+    dict_word = dictionary.meaning(eng_word)  # noun/verb/adj + description + too much info
+    for nav in dict_word:
+        if nav in ['Noun', 'Verb', 'Adjective', 'Adverb']:
+            Nav = nav  # noun, adj or verb
+
+    title = 'WWOTD: {}'.format(welsh_word)
+    selftext = '{0}  \n\n{1}'.format(eng_word, Nav)
+    reddit.subreddit('testingground4bots').submit(title, selftext)
+
+test_post()
