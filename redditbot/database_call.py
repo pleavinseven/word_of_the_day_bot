14e39c864d1c0236d63f4f7d1bf6409f1ff6c3a6
pleav <pleavinseven@gmail.com>
2021-07-17 02:09:02 +0200

    added config

--- redditbot/database_call.py
+++ redditbot/database_call.py
@@ -1,12 +1,13 @@
 import os
 import mysql.connector
+from config import config as cfg
+cfg_dict = cfg.config_dict
 
 mydb = mysql.connector.connect(
-    host=os.getenv('db_host'),
-    user=os.getenv('db_username'),
-    password=os.getenv('db_password'),
-    database=os.getenv('db_name')
-)
+    host=os.getenv(f'{cfg_dict["Database"]["db_host"]}'),
+    user=os.getenv(f'{cfg_dict["Database"]["db_username"]}'),
+    password=os.getenv(f'{cfg_dict["Database"]["db_password"]}'),
+    database=os.getenv(f'{cfg_dict["Database"]["db_name"]}'))
 
 
 def reset_database():
@@ -18,7 +19,7 @@ def reset_database():
 
 def DB_query():
     my_cursor = mydb.cursor(buffered=True)
-    sql = "SELECT * FROM words WHERE USED is False"
+    sql = "SELECT * FROM words WHERE Used is False"
     my_cursor.execute(sql)
     mydb.commit()
     my_result = my_cursor.fetchall()
