5aa1814040f91e56ed5be65f4a9fe32c884de87a
pleav <pleavinseven@gmail.com>
2021-07-17 02:08:26 +0200

    added config

---
+++ config/config.py
@@ -0,0 +1,14 @@
+import json
+
+config_file = "../config/config.json"
+config_dict = {}
+
+
+def load_config():
+    global config_dict
+
+    with open(config_file, "r") as file:
+        config_dict = json.load(file)
+
+
+load_config()
